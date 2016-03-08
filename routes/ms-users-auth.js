f2d043d9b9d190f1016d8174e18ff64172206471
Ryan Stanley <stanleyrya@gmail.com>
2016-03-08 03:12:09 -0500

    added routes from ms-users-auth with middlewear/lib auth.js

---
+++ routes/ms-users-auth.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../lib/db.js');
+var auth = require('../lib/auth.js');
+
+// edit user
+router.post('/', auth, function(req, res, next){
+  var location = req.body.location;
+  var id = req.user.sub;
+  if(username || location){
+    var qString = "UPDATE users SET location = $1 WHERE uid = $2";
+    db.query({text:qString, values: [location,id]}, function(err,success){
+      if(err){
+        res.send({
+          success: false,
+          error: err,
+          message: "server error"
+        });
+      } else{
+        res.send({
+          success: true,
+          message: 'location updated'
+        });
+      }
+    });
+  } else{
+    res.send({
+      success: false,
+      message: "no update requested"
+    });
+  }
+});
+
+// DELETE user TODO Admin only once we make admins.
+router.delete('/:username', auth, function(req, res, next) {
+  if(!req.user.admin){
+    res.send({
+      success:false,
+      message:'Must be an admin to delete users'
+    });
+    return;
+  }
+  var username = req.params.username;
+  var qString = 'DELETE FROM users WHERE username = $1';
+  db.query({text: qString, values: [username]}, function(err, success){
+    if(err) {
+      res.send(err.detail);
+    }
+    else {
+      res.send({
+        success: true,
+        message: "Deleted " + username
+      });
+    }
+  });
+});
+
+
+module.exports = router;
