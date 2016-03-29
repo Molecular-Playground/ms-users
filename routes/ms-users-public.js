d83a5a59a900073a12be39ec10907b894b8cf5e1
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-03-29 17:28:09 -0400

    Began implementing a resend-validation route

--- routes/ms-users-public.js
+++ routes/ms-users-public.js
@@ -6,6 +6,11 @@ var request = require('request');
 var MS_EMAIL_URL = "http://msemail:3000";
 var MS_FRONTEND_URL = "http://msfrontend:3000";
 
+// Used in / and /re-validate for generating validation URLs
+function randomString(length) {
+    return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
+}
+
 // get users listing
 router.get('/', function(req, res, next) {
   db.query('SELECT username FROM users', function(err, result){
@@ -31,9 +36,7 @@ router.get('/:username', function(req, res, next) {
 // create user
 router.put('/', function(req, res, next) {
   // http://stackoverflow.com/questions/10726909/random-alpha-numeric-string-in-javascript
-  function randomString(length) {
-    return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
-  }
+  
   var validationURL = randomString(30);
   var username =  req.body.username;
   var email =     req.body.email;
@@ -65,7 +68,7 @@ router.put('/', function(req, res, next) {
       });
     });
   } else {
-    var err = new Error("Didn't recieve required information");
+    var err = new Error("Required information has not been given. Ensure that a username, email, and password are all specified.");
     next(err);
   }
 });
@@ -100,7 +103,7 @@ router.post('/validate', function(req, res, next){
       } else { //if(results.rows[0] && (key === results.rows[0].validation_url))
         res.send({
           success: false,
-          message: "Invalid url"
+          message: "Validation url specified does not exist in the database."
         });
       }
     });
@@ -108,9 +111,62 @@ router.post('/validate', function(req, res, next){
   else { //if(email && key)
     res.send({
       success: false,
-      message: "Email or key not specified in body"
+      message: "Email or key not properly specified (this is most likely a front-end developer error)."
     });
   }
+});
+
+/*
+ * A route to resend the validation email for a specific user.
+ * Requires a body param containing the user's EMAIL.
+ * Returns success or failure to the calling function.
+ */
+router.post('/resend-validation', function(req, res, next){
+  var email = req.body.email;
+  // Generate a new validation URL for the user.
+  var validationURL = randomString(30);
+  if (email) {
+    var query = "SELECT * FROM users WHERE email = $1"
+    // First, get the user object from database.
+    db.query({text: query, values: [email]}, function(err, results){
+      if (err) {
+        res.send({success: false, message: "Database error", error: err});
+      }
+      else {
+        console.log(results);
+        // Assuming user exists, update their validation_url to
+        // the one we just created.
+        var insertQuery = "UPDATE users SET validation_url=$1 WHERE email=$2";
+        db.query({text: query, values:[validationURL, email]}, function(err, result){
+          if (err) {
+            res.send({success: false, message: "Database error", error: err});
+          }
+          else {
+            console.log(result);
+            // Prepare and send a request to ms-email
+            var reqParams = {
+                        url: MS_EMAIL_URL + '/validate',
+                        method: 'PUT',
+                        json: true,
+                        body: {
+                          email: email,
+                          link: MS_FRONTEND_URL + "/validate?email=" + email + "&key=" + validationURL
+                        }
+            };
+            request(reqParams, function (error, response, body) {
+              if(error) {
+                next(error);
+                return;
+              }
+              // If the request to me-email hasn't failed,
+              // Return a success status.
+              res.send({success: true, message: 'Successfully resent validation email.'});
+            }); // end request()
+          }
+        });
+      } // end else
+    }); // end db.query()
+  } // end if email
 });
 
 module.exports = router;
